0326ed31539e09b0af28dd9f627f209a863705c2
Gulia <gulya.isaeva.pls@gmail.com>
2024-09-11 15:08:18 +0300

    feat:add navbar page with styles

---
+++ src/components/authLinks/AuthLinks.jsx
@@ -0,0 +1,8 @@
+import React from "react";
+import styles from "./authLinks.module.css";
+
+function AuthLinks() {
+  return <div className={styles.container}>AuthLinks</div>;
+}
+
+export default AuthLinks;
---
+++ src/components/menu/Menu.jsx
@@ -0,0 +1,8 @@
+import React from "react";
+import styles from "./menu.module.css";
+
+function Menu() {
+  return <div className={styles.container}>menu</div>;
+}
+
+export default Menu;
